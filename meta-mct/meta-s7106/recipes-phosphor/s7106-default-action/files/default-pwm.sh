57bb3f5b2e1cb87decfc2e3f07532aa368060a5c
jeannie.wu <jeannie.wu@mic.com.tw>
2019-04-19 18:47:00 +0800

    [s7106] Modified default_pwm script.

--- meta-mct/meta-s7106/recipes-phosphor/s7106-default-action/files/default-pwm.sh
+++ meta-mct/meta-s7106/recipes-phosphor/s7106-default-action/files/default-pwm.sh
@@ -2,20 +2,8 @@
 
 default_duty=128
 
-list=`cat /etc/default/obmc/hwmon/ahb/apb/pwm-tacho-controller\@1e786000.conf | grep LABEL | awk -F '=' '{print $2}'`
-listarray=($list)
-
-for (( i=0; i<${#listarray[@]}; i++ ));do
-    fan_service=`mapper get-service /xyz/openbmc_project/sensors/fan_tach/${listarray[$i]}`
-    if [ "$?" = "0" ]; then
-       break
-    fi
-done
-
-fan_interface="xyz.openbmc_project.Control.FanPwm"
-property="Target"
-
-for (( i=0; i<${#listarray[@]}; i++ ));do
-    fan_object="/xyz/openbmc_project/sensors/fan_tach/${listarray[$i]}"
-    busctl set-property $fan_service $fan_object $fan_interface $property 't' $default_duty
-done
+echo $default_duty > /sys/class/hwmon/hwmon0/pwm1
+echo $default_duty > /sys/class/hwmon/hwmon0/pwm2
+echo $default_duty > /sys/class/hwmon/hwmon0/pwm3
+echo $default_duty > /sys/class/hwmon/hwmon0/pwm4
+echo $default_duty > /sys/class/hwmon/hwmon0/pwm5
