cafd4380803ba5b56aac88c8598e8660f1a0934e
jeannie.wu <jeannie@localhost.localdomain>
2019-06-14 17:20:37 +0800

    [s7106] Fix power control fail after sync     Summary:       1. Use gpioset/gpioget instead of gpioutil in power control script       2. Add obmcutil chassisoff to fix chassis status not sync after power off.
        Tested:
          1. Execute "ipmitool power on" and use "obmcutil status" to check power status
          2. Execute "ipmitool power off" and use "obmcutil status" to check power status

--- meta-mct/meta-s7106/recipes-phosphor/s7106-powerctrl/files/poweron.sh
+++ meta-mct/meta-s7106/recipes-phosphor/s7106-powerctrl/files/poweron.sh
@@ -2,22 +2,15 @@
 
 echo "Enter Power on System action"
 
-#pwrstatus=$(/usr/sbin/gpioutil -n D2 --getval)
+#USE GPIOD2 to check power status
 pwrstatus=$(/usr/bin/gpioget gpiochip0 26)
 if [ $pwrstatus -eq 0 ]; then
     # *** Push power button ***
-    GPIO_BASE=$(cat /sys/devices/platform/ahb/ahb:apb/1e780000.gpio/gpio/*/base)
-    # GPIO E1
-    GPIO_NUM=$(($GPIO_BASE + 32 + 1))
-    
-    echo ${GPIO_NUM} > /sys/class/gpio/export
-
-    echo "out" > /sys/class/gpio/gpio${GPIO_NUM}/direction
-    echo 0 > /sys/class/gpio/gpio${GPIO_NUM}/value
+    # GPIO E1 for power on
+    /usr/bin/gpioset gpiochip0 33=0
     sleep 2
-    echo 1 > /sys/class/gpio/gpio${GPIO_NUM}/value
-    
-    echo ${GPIO_NUM} > /sys/class/gpio/unexport
+    /usr/bin/gpioset gpiochip0 33=1 
+
 fi
 
 echo "Exit Power on System action"
