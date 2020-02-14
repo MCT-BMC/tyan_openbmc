ea0334d95e084c06ca8b7c4a5560b9b4a52fdc32
Ray Lue <ray.lue@mic.com.tw>
2020-02-14 22:08:29 +0800

    [f5i] Minor change for PHY configuraitn and host state - sync PHY LED register to F4. - reduce the host state change during BMC boot

--- meta-mct/meta-f5i/recipes-phosphor/gpio/phosphor-gpio-monitor/setPowerProperty.sh
+++ meta-mct/meta-f5i/recipes-phosphor/gpio/phosphor-gpio-monitor/setPowerProperty.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
+bmcState=$(busctl get-property xyz.openbmc_project.State.BMC /xyz/openbmc_project/state/bmc0 xyz.openbmc_project.State.BMC CurrentBMCState|cut -d ' ' -f 2|cut -d . -f 6|cut -c -5)
+
 if [ "$1" == "on" ]; then
     echo "pgood on"
     busctl set-property "org.openbmc.control.Power" "/org/openbmc/control/power0" "org.openbmc.control.Power" "pgood" i 1
     busctl set-property "org.openbmc.control.Power" "/org/openbmc/control/power0" "org.openbmc.control.Power" "state" i 1
-    busctl set-property xyz.openbmc_project.State.Host /xyz/openbmc_project/state/host0 xyz.openbmc_project.State.Host RequestedHostTransition s "xyz.openbmc_project.State.Host.Transition.On"
+    if [ $bmcState == "Ready" ]; then
+        busctl set-property xyz.openbmc_project.State.Host /xyz/openbmc_project/state/host0 xyz.openbmc_project.State.Host RequestedHostTransition s "xyz.openbmc_project.State.Host.Transition.On"
+    fi
+
 else
     echo "pgood off"
     busctl set-property "org.openbmc.control.Power" "/org/openbmc/control/power0" "org.openbmc.control.Power" "pgood" i 0
     busctl set-property "org.openbmc.control.Power" "/org/openbmc/control/power0" "org.openbmc.control.Power" "state" i 0
+    if [ $bmcState == "Ready" ]; then
     busctl set-property xyz.openbmc_project.State.Host /xyz/openbmc_project/state/host0 xyz.openbmc_project.State.Host RequestedHostTransition s "xyz.openbmc_project.State.Host.Transition.Off"
+    fi
 fi
 
