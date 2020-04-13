c78ace7d39a2380dd750b72bfd1fcf2bdeeb8888
wendy.shi <wendy.shi@mic.com.tw>
2020-04-13 19:14:22 +0800

    [f5i] Support Intel XXV710-DA1 OCP card.
    Test Plan:
    1. check XXV710-DA1 OCP card , temp reading -> PASS
     - system power on
         NIC1 Temp        | 36.000     | degrees C
     - system power off
         NIC1 Temp        | na         | degrees C
    
    2. check Mellanox OCP card , temp reading -> PASS
    
    Reviewers: ray.lue, john.chung
    
    Reviewed By: ray.lue
    
    Differential Revision: http://10.99.241.45/D1323

--- meta-mct/meta-f5i/recipes-phosphor/s7106-default-action/files/disable-sw2.sh
+++ meta-mct/meta-f5i/recipes-phosphor/s7106-default-action/files/disable-sw2.sh
@@ -18,7 +18,35 @@ property="Value"
 sensor_value=$(busctl get-property $service $object $interface $property | awk -F' ' '{print $2}')
 
 if [ -z "$sensor_value" ]; then
-    /usr/bin/gpioset gpiochip0 49=1
+    #/usr/bin/gpioset gpiochip0 49=1
     echo 0x1f > /sys/bus/i2c/devices/i2c-14/delete_device
-    echo " Didn't have OCP card , disable SW2"
+    echo " Didn't have switch port1 OCP card"
+
+    pservice="xyz.openbmc_project.State.Chassis"
+    pinterface="xyz.openbmc_project.State.Chassis"
+    pobject="/xyz/openbmc_project/state/chassis0"
+    pproperty="CurrentPowerState"
+    Power_state="Off"
+
+    while [ $Power_state != "On" ]; do
+        Power_state=$(busctl get-property $pservice $pobject $pinterface $pproperty | awk -F'"' '{print $2}' | awk -F'.' '{print $6}')
+        sleep 5
+    done
+
+    if [ ! -d "/sys/bus/i2c/devices/17-001f/hwmon" ]; then
+        echo " Dir .../17-001f/hwmon does not exists , create port4 OCP card "
+        echo 0x1f > /sys/bus/i2c/devices/i2c-17/delete_device
+        echo tmp75 0x1f > /sys/bus/i2c/devices/i2c-17/new_device
+        systemctl restart xyz.openbmc_project.hwmontempsensor.service
+
+        if [ ! -d "/sys/bus/i2c/devices/17-001f/hwmon" ]; then
+            echo 0x1f > /sys/bus/i2c/devices/i2c-17/delete_device
+            /usr/bin/gpioset gpiochip0 49=1
+            echo " Both OCP card does not exists , disable SW2 "
+        fi
+    fi
+else
+    echo 0x1f > /sys/bus/i2c/devices/i2c-17/delete_device
+    echo "Didn't have switch port4 OCP card"
 fi
+
